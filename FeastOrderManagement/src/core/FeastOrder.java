8cb3242d8214eef13095cf664ba77342e9c40a05
Cuong Lam-Quoc <cuonglam.fpt@gmail.com>
2025-02-01 17:54:58 +0700

    done well

--- FeastOrderManagement/src/core/FeastOrder.java
+++ FeastOrderManagement/src/core/FeastOrder.java
@@ -25,4 +25,50 @@ public class FeastOrder {
         this.evtDate = evtDate;
     }
 
+    public FeastOrder(int orderID) {
+        this.orderID = orderID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        FeastOrder fO = (FeastOrder) obj;
+        return this.orderID == fO.orderID;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public String getCustCode() {
+        return custCode;
+    }
+
+    public String getFstCode() {
+        return fstCode;
+    }
+
+    public int getNumOftables() {
+        return numOftables;
+    }
+
+    public Date getEvtDate() {
+        return evtDate;
+    }
+
+    public void setFstCode(String fstCode) {
+        this.fstCode = fstCode;
+    }
+
+    public void setNumOftables(int numOftables) {
+        this.numOftables = numOftables;
+    }
+
+    public void setEvtDate(Date evtDate) {
+        this.evtDate = evtDate;
+    }
+    
 }
